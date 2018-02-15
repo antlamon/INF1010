fdbb5c78f4591c826338cdf0ea72aecc84f35f3b
antoine lamontagne <antoine.lamontagne@hotmail.ca>
2018-02-15 13:48:13 -0500

    ajout td3

---
+++ TD3_1900664_1907605/TD3_1900664_1907605/ProduitAuxEncheres.cpp
@@ -0,0 +1,46 @@
+/********************************************
+* Titre: Travail pratique #3 - ProduitAuxEncheres.cpp
+* Date: 
+* Auteur:
+*******************************************/
+
+#include "ProduitAuxEncheres.h"
+
+ProduitAuxEncheres::ProduitAuxEncheres(Fournisseur & fournisseur, const string & nom, int reference, double prix, TypeProduit type)
+	:Produit(fournisseur, nom, reference, prix, type)
+{
+	identifiantClient_ = 0;
+	prixBase_ = 0;
+}
+
+int ProduitAuxEncheres::obtenirIdentifiantClient() const
+{
+	return identifiantClient_;
+}
+
+double ProduitAuxEncheres::obtenirPrixBase() const
+{
+	return prixBase_;
+}
+
+void ProduitAuxEncheres::modifierIdentifiantClient(int identifiantClient)
+{
+	identifiantClient_ = identifiantClient;
+}
+
+void ProduitAuxEncheres::modifierPrixBase(double prixBase)
+{
+	prixBase_ = prixBase;
+}
+
+istream & operator>>(istream & is, ProduitAuxEncheres & produit)
+{
+	return is >> static_cast<Produit&>(produit);
+}
+
+ostream & operator<<(ostream & os, const ProduitAuxEncheres & produit)
+{
+	os << "Produit au enchère " << static_cast<Produit>(produit)
+		<< " \t \t Prix de base: " << produit.prixBase_ << endl << " \t \t Identifiant client:" << produit.identifiantClient_ << endl;
+	return os;
+}
---
+++ TD3_1900664_1907605/TD3_1900664_1907605/ProduitOrdinaire.cpp
@@ -0,0 +1,39 @@
+/********************************************
+* Titre: Travail pratique #3 - ProduitOrdinaire.h
+* Date: 
+* Auteur:
+*******************************************/
+
+
+#include <string>
+#include <iostream>
+#include "ProduitOrdinaire.h"
+#include "Fournisseur.h"
+using namespace std;
+
+ProduitOrdinaire::ProduitOrdinaire(Fournisseur & fournisseur, const string & nom, int reference, double prix, TypeProduit type, bool estTaxable)
+	:Produit(fournisseur, nom, reference, prix, type), estTaxable_(estTaxable)
+{
+
+}
+
+bool ProduitOrdinaire::obtenirEstTaxable() const
+{
+	return estTaxable_;
+}
+
+void ProduitOrdinaire::modifierEstTaxable(bool estTaxable)
+{
+	estTaxable_ = estTaxable;
+}
+
+ostream & operator<<(ostream & os, const ProduitOrdinaire & produit)
+{
+	os << "Produit Ordinaire " << static_cast<Produit>(produit) << " \t \t est taxable : " << produit.estTaxable_ << endl;
+	return os;
+}
+
+istream & operator>>(istream & is, ProduitOrdinaire & produit)
+{
+	return is >> static_cast<Produit&>(produit) >> produit.estTaxable_;
+}
