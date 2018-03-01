d21b7cc1eb220065ec4ab1190cf54a9769d5d65e
Frédéric <frfork@labos.polymtl.ca>
2018-03-01 15:23:14 -0500

    Gestionnaire.cpp et.h complétés et comentés

--- TP4_1900664_1907605/TP4_1900664_1907605/Gestionnaire.h
+++ TP4_1900664_1907605/TP4_1900664_1907605/Gestionnaire.h
@@ -1,3 +1,9 @@
+/********************************************
+* Titre: Travail pratique #4 - Fournisseur.cpp
+* Date: 1 mars 2018
+* Auteur: Frédéric Fortin 1900664 Antoine Lamontagne 1907605
+*******************************************/
+
 #ifndef GESTIONNAIRE_H
 #define GESTIONNAIRE_H
 
@@ -11,15 +17,18 @@ using namespace std;
 class Gestionnaire
 {
   public:
+	//Méthode d'accès
     vector<Usager *> obtenirUsagers() const;
+
+	//Méthodes publiques
     void afficherLesProfils() const;
     double obtenirChiffreAffaires() const;
-
     void ajouterUsager(Usager *usager);
     void reinitialiser();
     void encherir(Client *client, ProduitAuxEncheres *produit, double montant) const;
 
   private:
+	  //Attributs
     vector<Usager *> usagers_;
 };
 
