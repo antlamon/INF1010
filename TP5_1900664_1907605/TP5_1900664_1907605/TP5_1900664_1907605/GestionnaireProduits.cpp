36e05ded5293b892b7075c9840f1fd7c2a70aedd
Fred <f.fortin1997@gmail.com>
2018-04-05 13:56:48 -0400

    Ajout des définitions

--- TP5_1900664_1907605/TP5_1900664_1907605/TP5_1900664_1907605/GestionnaireProduits.cpp
+++ TP5_1900664_1907605/TP5_1900664_1907605/TP5_1900664_1907605/GestionnaireProduits.cpp
@@ -5,3 +5,40 @@
 *******************************************/
 
 #include "GestionnaireProduits.h"
+
+void GestionnaireProduits::reinitialiserClient()
+{
+}
+
+void GestionnaireProduits::reinitialiserFournisseur()
+{
+}
+
+void GestionnaireProduits::afficher() const
+{
+}
+
+double GestionnaireProduits::obtenirTotalAPayer() const
+{
+	return 0.0;
+}
+
+double GestionnaireProduits::obtenirTotalApayerPremium() const
+{
+	return 0.0;
+}
+
+Produit * GestionnaireProduits::trouverProduitPlusCher() const
+{
+	return nullptr;
+}
+
+vector<pair<int, Produit*>> GestionnaireProduits::obtenirProduitsEntre() const
+{
+	return vector<pair<int, Produit*>>();
+}
+
+Produit * GestionnaireProduits::obtenirProduitSuivant(Produit * prod) const
+{
+	return nullptr;
+}
